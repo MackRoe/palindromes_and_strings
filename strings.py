28cdaaed75daad627f85a539c5fcdeb1aeeb9b7f
MackRoe <elaine.music@students.makeschool.com>
2020-04-14 18:10:46 -0700

    add code to contains method in strings.py

--- strings.py
+++ strings.py
@@ -1,10 +1,13 @@
 #!python
 
+
 def contains(text, pattern):
     """Return a boolean indicating whether pattern occurs in text."""
     assert isinstance(text, str), 'text is not a string: {}'.format(text)
     assert isinstance(pattern, str), 'pattern is not a string: {}'.format(text)
     # TODO: Implement contains here (iteratively and/or recursively)
+    if pattern in text:
+        return True
 
 
 def find_index(text, pattern):
